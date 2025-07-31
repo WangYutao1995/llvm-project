74435bc954a9c121371bc82073fb1807cd692e76
Yutao Wang <yutaowang2020@163.com>
2025-07-31 07:39:24 -0700

    [PATCH 5/6] Add AsmParser

--- llvm/lib/Target/M88k/MCTargetDesc/M88kMCTargetDesc.cpp
+++ llvm/lib/Target/M88k/MCTargetDesc/M88kMCTargetDesc.cpp
@@ -7,6 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "M88kMCTargetDesc.h"
+#include "M88kInstPrinter.h"
+#include "M88kMCAsmInfo.h"
 #include "TargetInfo/M88kTargetInfo.h"
 #include "llvm/MC/MCDwarf.h"
 #include "llvm/MC/MCInstrInfo.h"
@@ -25,6 +27,21 @@ using namespace llvm;
 
 #define GET_REGINFO_MC_DESC
 #include "M88kGenRegisterInfo.inc"
+
+static MCInstPrinter *createM88kMCInstPrinter(const Triple &T,
+		                              unsigned SyntaxVariant,
+					      const MCAsmInfo &MAI,
+					      const MCInstrInfo &MII,
+					      const MCRegisterInfo &MRI) {
+  return new M88kInstPrinter(MAI, MII, MRI);
+}
+
+static MCAsmInfo *createM88kMCAsmInfo(const MCRegisterInfo &MRI,
+		                      const Triple &TT,
+				      const MCTargetOptions &Options) {
+  MCAsmInfo *MAI = new M88kMCAsmInfo(TT);
+  return MAI;
+}
 
 static MCInstrInfo *createM88kMCInstrInfo() {
   MCInstrInfo *X = new MCInstrInfo();
@@ -45,13 +62,26 @@ static MCSubtargetInfo *createM88kMCSubtargetInfo(const Triple &TT,
 
 
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeM88kTargetMC() {
+  // Register the MCAsmInfo.
+  TargetRegistry::RegisterMCAsmInfo(getTheM88kTarget(), createM88kMCAsmInfo);
+
+  // Register the MCCodeEmitter.
+  TargetRegistry::RegisterMCCodeEmitter(getTheM88kTarget(),
+		                        createM88kMCCodeEmitter);
+
   // Register the MCInstrInfo.
   TargetRegistry::RegisterMCInstrInfo(getTheM88kTarget(),
 		                      createM88kMCInstrInfo);
+
   // Register the MCRegisterInfo.
   TargetRegistry::RegisterMCRegInfo(getTheM88kTarget(),
 		                    createM88kMCRegisterInfo);
+
   // Register the MCSubtargetInfo.
   TargetRegistry::RegisterMCSubtargetInfo(getTheM88kTarget(),
 		                          createM88kMCSubtargetInfo);
+
+  // Register the MCInstPrinter.
+  TargetRegistry::RegisterMCInstPrinter(getTheM88kTarget(),
+		                        createM88kMCInstPrinter);
 }
